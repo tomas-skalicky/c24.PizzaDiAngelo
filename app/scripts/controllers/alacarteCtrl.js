99eab1a147df01570d1b93481822b57a2e06df7a
Liviu Ignat <liviubiker@yahoo.com>
2013-12-02 23:26:08 +0100

    Added basic call and tests for BasketService. Styled a little bit the view

--- app/scripts/controllers/alacarteCtrl.js
+++ app/scripts/controllers/alacarteCtrl.js
@@ -1,18 +1,24 @@
 'use strict';
 
 angular.module('c24.PizzaDiAngeloApp.controllers', ['c24.PizzaDiAngeloApp.services'])
-	.controller('ALaCarteCtrl', function ($scope, $timeout, InventoryService) {
+	.controller('ALaCarteCtrl', function ($scope, $timeout, InventoryService, BasketService) {
 		$scope.pizzas = InventoryService.fetchBasePizzas();
 
 		$scope.addToBasket = function(pizza) {
 			//Should relly on Basket service. Wee need that service to proceed
 			var inBasketCount = (pizza.inBasketCount || 0) + 1;
 			pizza.inBasketCount = inBasketCount;
+
+			BasketService.addItem(pizza, 1);
+			console.log(BasketService.basket.items);
 		}
 
 		$scope.extractFromBasket = function(pizza) {
 			//Should relly on Basket service. Wee need that service to proceed
 			var inBasketCount = (pizza.inBasketCount || 0) - 1;
 			pizza.inBasketCount = inBasketCount;
+
+			BasketService.removeItem(pizza);
+			console.log(BasketService.basket.items);
 		}
 	});
--- test/spec/controllers/alacarteCtrlSpec.js
+++ test/spec/controllers/alacarteCtrlSpec.js
@@ -2,25 +2,29 @@
 
 describe('Controller: ALaCarteCtrl', function(){
   var $controllerService, $scope,
-  	controller,
-	pizzasMockResponse = [ { id: 1, name: 'Thin Crust' } ],
-	mockInventoryService;
+    controller,
+    pizzasMockResponse,
+	  mockInventoryService,
+    basketService;
 
   beforeEach(function () {
     module('c24.PizzaDiAngeloApp');
     module('c24.PizzaDiAngeloApp.controllers');
   });
 
-  beforeEach(inject(function($controller, $rootScope){
+  beforeEach(inject(function($injector, $controller, $rootScope){
+    pizzasMockResponse = [ { id: 1, name: 'Thin Crust' } ];
   	$controllerService = $controller;
     $scope = $rootScope.$new();
 
     mockInventoryService = sinon.stub({ fetchBasePizzas: function () {} });
+    basketService = $injector.get('BasketService');
 
     mockInventoryService.fetchBasePizzas.returns(pizzasMockResponse);
-	controller = $controllerService('ALaCarteCtrl', {
+    controller = $controllerService('ALaCarteCtrl', {
       $scope: $scope,
-      InventoryService: mockInventoryService
+      InventoryService: mockInventoryService,
+      BasketService: basketService
     });
   }));
 
@@ -28,16 +32,38 @@ describe('Controller: ALaCarteCtrl', function(){
   	expect($scope.pizzas).toBe(pizzasMockResponse);
   });
 
-  it('Pizzas added should increase pizza.inBasketCount', function(){
-  	var pizza = $scope.pizzas[0];
-  	$scope.addToBasket(pizza);
-  	expect(pizza.inBasketCount).toBe(1);
+  describe('When adding pizzas', function() {
+    it('Pizzas added should increase pizza.inBasketCount', function(){
+      var pizza = $scope.pizzas[0];
+      $scope.addToBasket(pizza);
+      expect(pizza.inBasketCount).toBe(1);
+    });
+
+    it('BasketService.addItem should be called', function(){
+      var pizza = $scope.pizzas[0],
+        spy = spyOn(basketService, 'addItem');
+
+      $scope.addToBasket(pizza);
+
+      expect(spy.wasCalled).toBe(true);
+    });
   });
 
-  it('Pizzas added should decrease pizza.inBasketCount', function(){
-  	var pizza = $scope.pizzas[0];
-  	pizza.inBasketCount = 10;
-  	$scope.extractFromBasket(pizza);
-  	expect(pizza.inBasketCount).toBe(9);
+  describe('When removing pizzas', function() {
+    it('Pizzas added should decrease pizza.inBasketCount', function(){
+      var pizza = $scope.pizzas[0];
+      pizza.inBasketCount = 10;
+      $scope.extractFromBasket(pizza);
+      expect(pizza.inBasketCount).toBe(9);
+    });
+
+    it('BasketService.removeItem should be called', function(){
+      var pizza = $scope.pizzas[0],
+        spy = spyOn(basketService, 'removeItem');
+
+      $scope.extractFromBasket(pizza);
+
+      expect(spy.wasCalled).toBe(true);
+    });
   });
 });
